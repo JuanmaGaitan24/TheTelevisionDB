25f53fd3bad7becab96a6dfd41edf3a0d94654fb
JuanmaGaitan24 <juanmanuelgaitan@iesflorenciopintado.es>
2023-01-06 18:33:30 +0100

    The Television DB V2.2

--- app/src/main/java/com/example/thetelevisiondb/ProgramacionActivity.java
+++ app/src/main/java/com/example/thetelevisiondb/ProgramacionActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -35,7 +38,15 @@ public class ProgramacionActivity extends AppCompatActivity {
             try {
 
                 DocumentBuilder db = dbf.newDocumentBuilder();
-                url = new URL("");
+                url = new URL("https://www.tdtchannels.com/epg/TV.xmlh");
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    Document doc = db.parse(httpURLConnection.getInputStream());
+                    Element root = doc.getDocumentElement();
+                    NodeList items = root.getElementsByTagName("programme");
+                    ArrayProgramas = new SeriesActivity.Serie[items.getLength()];
+                }
 
             } catch (MalformedURLException e){
                 e.printStackTrace();
